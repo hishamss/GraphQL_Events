722fe08ff1f4b3d81a7908b9028c1f4ce6d98e3c
hishamss <hishamss90@gmail.com>
2020-08-13 20:13:53 -0700

    Test graphQL with hard coded data

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const events = [];
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+// Note: ! in garaphQL this means null is not allowed
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String! 
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String! 
+    }
+    type RootQuery {
+        events: [Event!]!
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    // Resolver
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          // to make sure whatever I type will converted to number
+          price: +args.eventInput.price,
+          data: args.eventInput.data,
+        };
+
+        events.push(event);
+        return event;
+      },
+    },
+    graphiql: true,
+  })
+);
 app.listen(PORT, () => {
   console.log(`app is listening on Port ${PORT}`);
 });
