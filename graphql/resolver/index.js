f7370915f2a32277aeddafed48d1dde82fa54916
hishamss <hishamss90@gmail.com>
2020-08-14 21:57:11 -0700

    changed from return to async-await

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,86 +1,85 @@
 const db = require("../../models");
 const bcrypt = require("bcrypt");
 module.exports = {
-  users: () => {
-    return db.Users.find()
-      .populate("createdEvents")
-      .then((result) => {
-        return result.map((row) => {
-          return {
-            ...row._doc,
-          };
-        });
-      })
-      .catch((err) => {
-        throw err;
+  users: async () => {
+    try {
+      let result = await db.Users.find().populate("createdEvents");
+      return result.map((row) => {
+        return {
+          ...row._doc,
+          password: null,
+        };
       });
+    } catch (err) {
+      throw err;
+    }
   },
-  events: () => {
-    return db.Events.find()
-      .populate("creator")
-      .then((result) => {
-        return result.map((row) => {
-          return {
-            ...row._doc,
-            creator: {
-              ...row._doc.creator._doc,
-              password: null,
-            },
-          };
-        });
-      })
-      .catch((err) => {
-        throw err;
+  events: async () => {
+    try {
+      let results = await db.Events.find().populate("creator");
+      return results.map((row) => {
+        return {
+          ...row._doc,
+          date: new Date(row._doc.date).toISOString(),
+          creator: {
+            ...row._doc.creator._doc,
+            password: null,
+          },
+        };
       });
+    } catch (err) {
+      throw err;
+    }
   },
-  createEvent: (args) => {
-    return db.Events.create({
-      title: args.eventInput.title,
-      description: args.eventInput.description,
-      price: +args.eventInput.price,
-      date: new Date(args.eventInput.date),
-      creator: "5f371e5b4d07a27630b64c64",
-    })
-      .then((result) => {
-        return db.Users.findOneAndUpdate(
+  createEvent: async (args) => {
+    try {
+      var result = await db.Events.create({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date),
+        creator: "5f371e5b4d07a27630b64c64",
+      });
+      try {
+        var userResult = await db.Users.findOneAndUpdate(
           { _id: "5f371e5b4d07a27630b64c64" },
           //   Note: mongoose will only get the id of the new added event object and push it to createdEvents array
           { $push: { createdEvents: result } }
-        )
-          .then(() => {
-            // between the following curly braces is recommended from mongoDB to convert the id to string
-            return { ...result._doc };
-          })
+        );
 
-          .catch((err) => {
-            throw err;
-          });
-      })
-      .catch((err) => console.log(err));
+        // between the following curly braces is recommended from mongoDB to convert the id to string
+        return {
+          ...result._doc,
+          //   date: new Date(result._doc.date).toISOString(),
+        };
+      } catch (err) {
+        throw err;
+      }
+    } catch (err) {
+      console.log(err);
+    }
   },
-  createUser: (args) => {
-    return bcrypt
-      .hash(args.userInput.password, 12)
-      .then((hashedPassword) => {
-        return db.Users.create({
+  createUser: async (args) => {
+    try {
+      let hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+      try {
+        let result = await db.Users.create({
           email: args.userInput.email,
           password: hashedPassword,
-        })
-          .then((result) => {
-            //   add password: null to prevent the user from getting the password after creating new user
-            return {
-              ...result._doc,
-              password: null,
-            };
-          })
-          .catch((err) => {
-            if (err.message.includes("E11000 duplicate key error collection")) {
-              throw "User Already Exists";
-            }
-          });
-      })
-      .catch((err) => {
-        throw err;
-      });
+        });
+
+        //   add password: null to prevent the user from getting the password after creating new user
+        return {
+          ...result._doc,
+          password: null,
+        };
+      } catch (err) {
+        if (err.message.includes("E11000 duplicate key error collection")) {
+          throw "User Already Exists";
+        }
+      }
+    } catch (err) {
+      throw err;
+    }
   },
 };
