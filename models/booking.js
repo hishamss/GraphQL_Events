68e541a36ed9c37e94d53a4082260b58760ca74b
hishamss <hishamss90@gmail.com>
2020-08-18 17:55:41 -0700

    node model for booking events

---
+++ models/booking.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema(
+  {
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "event",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+// Export the model to app.js
+module.exports = mongoose.model("event", eventSchema);
