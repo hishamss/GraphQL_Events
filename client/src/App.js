09ee4b2507b7cc12232c01a98b58b1f9627333f1
hishamss <hishamss90@gmail.com>
2020-09-05 16:14:26 -0700

    installed react router and implemented the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Login from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>It works!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={Login} />
+        <Route path="/events" component={null} />
+        <Route path="/bookings" component={null} />
+      </Switch>
+    </Router>
   );
 }
 
