534be8e344483981cd0b249478e57ae53c2f9c4a
hishamss <hishamss90@gmail.com>
2020-09-06 16:11:36 -0700

    created and styled the login form

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "./login.css";
 function loginPage() {
-  return <h1>This is Login page</h1>;
+  return (
+    <div className="authFormContainer">
+      <form className="authForm">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Email"></input>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" placeholder="Password"></input>
+        </div>
+        <button type="submit">Submit</button>
+        <button type="button">Switch to Singup</button>
+      </form>
+    </div>
+  );
 }
 
 export default loginPage;
