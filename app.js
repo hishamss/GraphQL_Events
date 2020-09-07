7aa5a5d31c41777b57749897bd8da71061a90984
hishamss <hishamss90@gmail.com>
2020-09-06 17:55:55 -0700

    added backend proxy on react server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 const graphQlSchema = require("./graphql/schema");
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./login.css";
-function loginPage() {
+
+function LoginPage() {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const Email = emailRef.current.value.trim();
+    const Password = passwordRef.current.value.trim();
+    if (!Email || !Password || !/@/.exec(Email)) {
+      setMessage("Missing Email/password Or Invalid Email");
+    } else {
+      setMessage("Submitted");
+    }
+  };
   return (
     <div className="authFormContainer">
-      <form className="authForm">
+      <form onSubmit={handleSubmit} className="authForm">
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="Email"></input>
+          <input
+            ref={emailRef}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+          ></input>
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="text" id="password" placeholder="Password"></input>
+          <input
+            ref={passwordRef}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          ></input>
         </div>
         <button type="submit">Submit</button>
         <button type="button">Switch to Singup</button>
+        <p>{message}</p>
       </form>
     </div>
   );
 }
 
-export default loginPage;
+export default LoginPage;
