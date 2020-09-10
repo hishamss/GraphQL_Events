df6471985c4b62dec2bfe6596eed8c16cee9f7f1
hishamss <hishamss90@gmail.com>
2020-09-09 20:22:00 -0700

    added modal for creating events

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,7 +1,32 @@
-import React from "react";
-
-function eventsPage() {
-  return <h1>This is Events page</h1>;
+import React, { useState } from "react";
+import "./events.css";
+function EventsPage() {
+  const [hideModal, setHideModal] = useState(true);
+  return (
+    <>
+      <div className="eventsContainer">
+        <button id="createEvent" onClick={() => setHideModal(false)}>
+          Add Event
+        </button>
+      </div>
+      <div
+        className={
+          hideModal ? "modalContainer hideModal" : " modalContainer showModal"
+        }
+      >
+        <div className="modal">
+          <div className="modalHeader">
+            <h1>Add Events</h1>
+          </div>
+          <div className="modalBody">
+            <p>content</p>
+            <button>Submit</button>
+            <button onClick={() => setHideModal(true)}>Cancel</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default eventsPage;
+export default EventsPage;
